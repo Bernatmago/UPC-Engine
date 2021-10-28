4f5476aa77cd3ba5e488bfcc9c81d599677591a2
Bernat Martinez <bolicubo@gmail.com>
2021-10-28 20:59:39 +0200

    Added opengl context

--- Source/ModuleRender.cpp
+++ Source/ModuleRender.cpp
@@ -4,6 +4,8 @@
 #include "ModuleWindow.h"
 #include "SDL.h"
 
+#include <gl/glew.h>
+
 ModuleRender::ModuleRender()
 {
 }
@@ -18,7 +20,25 @@ bool ModuleRender::Init()
 {
 	LOG("Creating Renderer context");
 
-	//glewInit();
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4); // desired version
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_COMPATIBILITY);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1); // we want a double buffer
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24); // we want to have a depth buffer with 24 bits
+	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8); // we want to have a stencil buffer with 8 bits
+
+	context = SDL_GL_CreateContext(App->window->window);
+
+	GLenum err = glewInit();
+	LOG("Using Glew %s", glewGetString(GLEW_VERSION));
+	LOG("Vendor: %s", glGetString(GL_VENDOR));
+	LOG("Renderer: %s", glGetString(GL_RENDERER));
+	LOG("OpenGL version supported %s", glGetString(GL_VERSION));
+	LOG("GLSL: %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
+
+	glEnable(GL_DEPTH_TEST); // Enable depth test
+	glEnable(GL_CULL_FACE); // Enable cull backward faces
+	glFrontFace(GL_CCW); // Front faces will be counter clockwise
 
 
 	return true;
@@ -26,6 +46,12 @@ bool ModuleRender::Init()
 
 update_status ModuleRender::PreUpdate()
 {
+	int w, h;
+	SDL_GetWindowSize(App->window->window, &w, &h);
+	glViewport(0, 0, w, h);
+	glClearColor(0.8f, 0.1f, 0.1f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
 	return UPDATE_CONTINUE;
 }
 
@@ -38,6 +64,7 @@ update_status ModuleRender::Update()
 
 update_status ModuleRender::PostUpdate()
 {
+	SDL_GL_SwapWindow(App->window->window);
 	return UPDATE_CONTINUE;
 }
 
@@ -46,7 +73,7 @@ bool ModuleRender::CleanUp()
 {
 	LOG("Destroying renderer");
 
-	//Destroy window
+	SDL_GL_DeleteContext(context);
 
 	return true;
 }
